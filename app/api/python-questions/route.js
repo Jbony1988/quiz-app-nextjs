09457c11da75ae03f25599d3a49553a9b7fd8f58
Jerry bony <jbony@icstars.org>
2024-06-17 15:18:32 -0300

    pull data from database

--- app/api/python-questions/route.js
+++ app/api/python-questions/route.js
@@ -1,11 +1,11 @@
 import connectDB from '../../../config/database';
-import Quiz from '../../../models/Questions'; // Assuming the correct model name is QuizModel
-
+import Question from '../../../models/Questions'; // Assuming the correct model name is QuizModel
+import Property from '../../../models/Property'
 export const GET = async (request) => {
   try {
-    // await connectDB();
-    const quiz = await Quiz.find({});
-    return new Response(JSON.stringify({ message: quiz }), { status: 200 });
+    await connectDB();
+    const questions = await Question.find({});
+    return new Response(JSON.stringify({ message: questions }), { status: 200 });
   } catch (error) {
     console.error('Error fetching quiz data:', error);
     return new Response('Something went wrong', { status: 500 });
--- config/database.js
+++ config/database.js
@@ -1,28 +1,29 @@
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.MONGODB_URI;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = process.env.MONGODB_URI;
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//     bufferTimeoutMS: 0, 
+//   }
+// });
 
- async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!", client);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+//  async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!", client);
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 
 
@@ -47,3 +48,28 @@ run().catch(console.dir);
 // export default connectDB;
 
 
+import mongoose from 'mongoose';
+
+let connected = false;
+
+const connectDB = async () => {
+  mongoose.set('strictQuery', true);
+
+  // If the database is already connected, don't connect again
+  if (connected) {
+    console.log('MongoDB is already connected...');
+    return;
+  }
+
+  // Connect to MongoDB
+  try {
+    
+    await mongoose.connect(process.env.MONGODB_URI);
+    connected = true;
+    console.log('MongoDB connected...');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default connectDB;
