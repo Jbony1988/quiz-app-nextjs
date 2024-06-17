3369b38f59fa2b06dabf2f3a251d8894df054bd7
Jerry bony <jbony@icstars.org>
2024-06-16 22:17:40 -0300

    comment out mongodb

--- config/database.js
+++ config/database.js
@@ -1,33 +1,33 @@
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.MONGODB_URI;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = process.env.MONGODB_URI;
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
 
 
-const connectDB = async () => {
+// const connectDB = async () => {
 
 
-  // If the database is already connected, don't connect again
+//   // If the database is already connected, don't connect again
 
 
-  // Connect to MongoDB
-  try {
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } catch (error) {
-    await client.close();
-  }
-};
+//   // Connect to MongoDB
+//   try {
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } catch (error) {
+//     await client.close();
+//   }
+// };
 
-export default connectDB;
+// export default connectDB;
 
 
