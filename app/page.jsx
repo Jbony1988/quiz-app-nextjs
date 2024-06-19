34621fa4c3893589c3f2c2d9118c6e2e0b245e90
Jerry bony <jbony@icstars.org>
2024-06-19 15:56:32 -0300

    fix module url

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,7 @@
 import Home from "./-components/Home";
-import Navbar from './-components/Navbar'
-import connectDB from '../config/database'
+
 
 const HomePage  = async  () => {
-  // await connectDB()
 
   return (
     <div>
